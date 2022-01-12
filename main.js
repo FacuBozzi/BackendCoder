cbb208eb60db43e723960bf7db2b91e43a28e544
Facundo <facundo.bo@hotmail.com>
2022-01-11 21:18:52 -0300

    clase cuatro no terminada

--- main.js
+++ main.js
@@ -1,9 +1,75 @@
+const fs = require("fs")
+
 class Contenedor {
 
-    function save(archivo) {
-        var contenido = document.getElementById("editor").value;
-        var blob = new Blob([contenido], {type: "text/plain;charset=utf-8"});
-        saveAs(blob, archivo);
+    constructor(archivo, id){
+        this.archivo = archivo
+        this.id = id
+    }
+
+
+    save(archivo) {
+        fs.writeFile("./texto.txt", archivo, error => {
+            if (error) {
+                console.log(error)
+            } else {
+                console.log("Archivo guardado exitosamente")
+                return this.id + 1
+            }
+        })
+    }
+
+    getById(id) {
+        fs.readFile("./texto.txt", (error, data) => {
+            if (error) {
+                console.log(error)
+            } else {
+                data == id 
+                id = this.id + 1
+                return data
+            }
+        })
+    }
+
+    getAll() {
+        fs.readFile("./texto.txt", (error, data) => {
+            if (error) {
+                console.log(error)
+            } else {
+                id = this.id + 1
+                return data
+            }
+        })  
+    }
+
+    deleteById(id) {
+        fs.readFile("./texto.txt", (error, data) => {
+            if (error) {
+                console.log(error)
+            } else {
+                if (data == id) {
+                    return fs.unlink("./texto.txt", error => {
+                        if (error) {
+                            console.log(error)
+                        } else {
+                            id = this.id + 1
+                            console.log("Archivo eliminado exitosamente")
+                        }
+                    })
+                }
+            }
+        })
+    }
+
+    deleteAll() {
+        fs.unlink("./texto.txt", error => {
+            if (error) {
+                console.log(error)
+            } else {
+                id = this.id + 1
+                console.log("Archivo eliminado exitosamente")
+            }
+        })
     }
 
 }
